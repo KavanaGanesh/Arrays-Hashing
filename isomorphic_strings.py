6d0a7d836c2fe106f6868c49fed7c91cf796580a
Kavana Kiran <kavanakiran@Kavanas-MacBook-Pro.local>
2025-02-22 14:52:12 -0800

    Added complexities

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,4 +1,18 @@
+"""Isomorphic strings: strings that can be consistently replaced """
+'''"egg" and "add" are isomorphic because you can map 'e' to 'a', 'g' to 'd'''
 
+# conditions to check:
+# 1. char can be present as a key {p:t} - True ideal this is what i am considering
+# 2. the same char can be pointed to another char as a value{p:f} - this is False
+# 3. the same char can be present as a value mapped to another char as key {a:p} - this is False too
+
+# 1) check if both the length of strings doesnot match  - return False
+# 2) if the length matches: now create another dictionary, to map as the key - value pair using zip(s1,s2) 
+# 3) now all char in s1 are keys: with this condition:
+    # 4) check if each char is not mapping to y -  return False
+# 5) if the same key is also present in values - return False  
+# 6) just add the unique key-value to makiing it a proper key-value dictionary
+# 7) Finally return True
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -11,7 +25,7 @@ class Solution:
                 if order_dict[x] != y:
                     return False
             else:
-                if y in order_dict.values():
+                if x in order_dict.values():
                     return False
             order_dict[x] = y
         return True
@@ -21,6 +35,8 @@ class Solution:
 # t = "add"
 # s = "foo"
 # t = "bar"
-s = "paper"
-t = "title"
+# s = "paper"
+# t = "title"
+s = "aab" 
+t = "xxy"
 print(Solution().isIsomorphic(s,t))
