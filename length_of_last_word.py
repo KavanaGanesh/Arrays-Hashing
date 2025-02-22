6f3c13011c43bb26acef36cad54c1db62f9baf6b
Kavana Kiran <kavanakiran@Kavanas-MacBook-Pro.local>
2025-02-22 15:16:15 -0800

    featuring the complexities

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,3 +1,8 @@
+# 1)split and get the last word of the sentence - return value of the split is list[words]
+# 2)call the length of the last word
+
+# space and time complexities both are O(n) as it is goign to create the substring.
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return (len(s.split( )[-1]))
