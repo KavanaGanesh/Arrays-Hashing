7f149524810b11a673d40ddf21ec7bad190cb988
Kavana Kiran <kavanakiran@Mac.hsd1.ca.comcast.net>
2025-02-23 00:14:15 -0800

    featuring complexities

--- ransom_note.py
+++ ransom_note.py
@@ -1,3 +1,12 @@
+# 1) magazine has more number of characters and from magazine check if ransomenote can be made
+# 2) so pass the magazine into counter from collections module
+# 3) now pick each character and keep decrementing the count correpsonding to character
+# 4) when the count of the char becomes less than 0  - that shows ransomenote cannnot be made from magazine return False
+# 5) Finally return True
+
+# complexity: Time = O(n)
+# space = O(n)
+
 from collections import Counter
 
 class Solution:
@@ -10,11 +19,11 @@ class Solution:
                 return False
         return True
 
-# ransomNote = "a"
-# magazine = "b"
+ransomNote = "a"
+magazine = "b"
 # ransomNote = "aa"
 # magazine = "ab"
-ransomNote = "aa"
-magazine = "aab"
+# ransomNote = "aa"
+# magazine = "aab"
 print(Solution().canConstruct(ransomNote,magazine))
         
